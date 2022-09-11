a651bec76dd4d14796147fb11b9a6d4d73d8a24a
Noe Jupiter <noeonjupiter@icloud.com>
2022-09-11 15:46:12 +0300

    est

--- Sources/ScrollViewStyle/Views/ScrollViewStyleModifider.swift
+++ Sources/ScrollViewStyle/Views/ScrollViewStyleModifider.swift
@@ -15,16 +15,18 @@ struct ScrollViewStyleModifider<Style: ScrollViewStyle>: ViewModifier {
     let style: Style
     func body(content: Content) -> some View {
         style.makeBody(context: context) {
-            GeometryReader { proxy in
-                content
-                    .introspectScrollView { scrollView in
-                        style.makeUIScrollView(scrollView)
-                        scrollView.delegate = coordinator
-                    }.onChange(of: proxy) { newValue in
-                        context.proxy = newValue
-                    }.onChange(of: coordinator.offset) { newValue in
-                        context.offset = newValue
-                    }
+            VStack {
+                GeometryReader { proxy in
+                    content
+                        .introspectScrollView { scrollView in
+                            style.makeUIScrollView(scrollView)
+                            scrollView.delegate = coordinator
+                        }.onChange(of: proxy) { newValue in
+                            context.proxy = newValue
+                        }.onChange(of: coordinator.offset) { newValue in
+                            context.offset = newValue
+                        }
+                }
             }
         }
     }
