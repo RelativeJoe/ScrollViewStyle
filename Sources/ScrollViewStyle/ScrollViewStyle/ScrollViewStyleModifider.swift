985ce97293be0d07fb62b169fcba932be742112a
Noe Jupiter <noeonjupiter@icloud.com>
2022-09-11 18:32:05 +0300

    Te

--- Sources/ScrollViewStyle/ScrollViewStyle/ScrollViewStyleModifider.swift
+++ Sources/ScrollViewStyle/ScrollViewStyle/ScrollViewStyleModifider.swift
@@ -15,23 +15,44 @@ struct ScrollViewStyleModifider<Style: ScrollViewStyle>: ViewModifier {
     let style: Style
     func body(content: Content) -> some View {
         style.makeBody(context: context) {
-            GeometryReader { proxy in
-                content
-                    .introspectScrollView { scrollView in
-                        style.makeUIScrollView(scrollView)
-                        scrollView.delegate = coordinator
-                    }.change(of: proxy) { newValue in
-                        context.proxy = newValue
-                    }.change(of: coordinator.offset) { newValue in
-                        context.offset = newValue
-                    }
-            } as! Style.Content
+            WrapperView(content: content, coordinator: coordinator, context: $context, style: style) as! Style.Content
+//            GeometryReader { proxy in
+//                content
+//                    .introspectScrollView { scrollView in
+//                        style.makeUIScrollView(scrollView)
+//                        scrollView.delegate = coordinator
+//                    }.change(of: proxy) { newValue in
+//                        context.proxy = newValue
+//                    }.change(of: coordinator.offset) { newValue in
+//                        context.offset = newValue
+//                    }
+//            } as! Style.Content
         }
     }
 }
 
 public extension ScrollView {
-    @ViewBuilder func scrollViewStyle<Style: ScrollViewStyle>(_ style: Style) -> some View where Style.Content == GeometryReader<Self> {
+    @ViewBuilder func scrollViewStyle<Style: ScrollViewStyle>(_ style: Style) -> some View where Style.Content == WrapperView<Self, Style> {
         self.modifier(ScrollViewStyleModifider(style: style))
+    }
+}
+
+public struct WrapperView<Content: View, Style: ScrollViewStyle>: View {
+    var content: Content
+    @ObservedObject var coordinator: ScrollViewCoordinator
+    @Binding var context: Context
+    let style: Style
+    public var body: some View {
+        GeometryReader { proxy in
+            content
+                .introspectScrollView { scrollView in
+                    style.makeUIScrollView(scrollView)
+                    scrollView.delegate = coordinator
+                }.change(of: proxy) { newValue in
+                    context.proxy = newValue
+                }.change(of: coordinator.offset) { newValue in
+                    context.offset = newValue
+                }
+        }
     }
 }
