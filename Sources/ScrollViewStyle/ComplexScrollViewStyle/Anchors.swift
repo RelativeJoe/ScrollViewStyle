4d0d973f2673311dab08461c0ede071a745d079f
Noe Jupiter <noeonjupiter@icloud.com>
2022-09-12 20:26:00 +0300

    Testing

--- Sources/ScrollViewStyle/ComplexScrollViewStyle/Anchors.swift
+++ Sources/ScrollViewStyle/ComplexScrollViewStyle/Anchors.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-extension View {
+public extension View {
     @ViewBuilder func anchor<T: CustomStringConvertible>(id: T) -> some View {
         GeometryReader { reader in
             self
