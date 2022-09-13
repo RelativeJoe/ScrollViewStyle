4b7f29e762a935ea773fcac533a2d180effc0a49
Noe Jupiter <noeonjupiter@icloud.com>
2022-09-13 20:18:11 +0300

    Test

--- Sources/ScrollViewStyle/ComplexScrollViewStyle/ComplexScrollViewStyle.swift
+++ Sources/ScrollViewStyle/ComplexScrollViewStyle/ComplexScrollViewStyle.swift
@@ -9,7 +9,7 @@ import Foundation
 import SwiftUI
 
 @available(iOS 14.0, *)
-public struct ComplexScrollViewStyle<Content: View>: ScrollViewStyle {
+public struct ComplexScrollViewStyle: ScrollViewStyle {
     public func make(body: AnyView, context: Context) -> some View {
         body
             .preference(key: OffsetPreferenceKey.self, value: context)
--- Sources/ScrollViewStyle/ScrollViewStyle/ScrollViewCoordinator.swift
+++ Sources/ScrollViewStyle/ScrollViewStyle/ScrollViewCoordinator.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-public final class ScrollViewCoordinator: NSObject, UIScrollViewDelegate, ObservableObject {
+public class ScrollViewCoordinator: NSObject, UIScrollViewDelegate, ObservableObject {
     @Published public var offset: CGFloat = 0
     public func scrollViewDidScroll(_ scrollView: UIScrollView) {
         self.offset = scrollView.contentOffset.y
