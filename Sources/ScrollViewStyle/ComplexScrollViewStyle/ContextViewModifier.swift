3cb8fa0aa75de19f003ff3a71b0d75d4d6a3f285
Noe Jupiter <noeonjupiter@icloud.com>
2022-09-16 21:54:33 +0300

    Test

--- Sources/ScrollViewStyle/ComplexScrollViewStyle/ContextViewModifier.swift
+++ Sources/ScrollViewStyle/ComplexScrollViewStyle/ContextViewModifier.swift
@@ -9,21 +9,20 @@ import SwiftUI
 
 internal struct ContextViewModifier: ViewModifier {
     @State internal var context: Context?
-    @State internal var anchors = [ReaderAnchor]()
     internal func body(content: Content) -> some View {
         content
             .environment(\.prefrenceContext, context)
             .onPreferenceChange(OffsetPreferenceKey.self) { value in
                 var valuey = value
-                valuey?.anchors = anchors
+                valuey?.anchors = context?.anchors ?? []
                 context = valuey
             }.onPreferenceChange(ReaderPreferenceKey.self) { value in
                 guard let anchor = value else {return}
-                guard let index = anchors.firstIndex(of: anchor) else {
-                    anchors.append(anchor)
+                guard let index = context?.anchors.firstIndex(of: anchor) else {
+                    context?.anchors.append(anchor)
                     return
                 }
-                anchors[index] = anchor
+                context?.anchors[index] = anchor
             }
     }
 }
