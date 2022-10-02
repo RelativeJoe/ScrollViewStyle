7006886528448d1c9fb5aee8ae173d978f273d6a
Noe Jupiter <noeonjupiter@icloud.com>
2022-10-02 09:58:36 +0300

    ttt

--- Sources/ScrollViewStyle/ComplexScrollViewStyle/OffsetViewModifier.swift
+++ Sources/ScrollViewStyle/ComplexScrollViewStyle/OffsetViewModifier.swift
@@ -151,10 +151,10 @@ extension OffsetViewModifier: ViewModifier {
                                             if let anchor = paddingValue.anchor {
                                                 guard let anchorView = context.anchors.first(where: {$0.anchor == anchor}), let point = anchorView.reader?.frame(in: .global).getValue(paddingValue.position ?? .max, axis: paddingValue.axis ?? defaultAxis) else {return}
                                                 let viewPoint = proxy.frame(in: .global).getValue(paddingValue.position ?? .max, axis: paddingValue.axis ?? defaultAxis)
-                                                if !paddingValue.invertedOffset && viewPoint > point {
+                                                if !paddingValue.invertedOffset && viewPoint < point {
                                                     padding.1 -= viewPoint - point
                                                     return
-                                                }else if paddingValue.invertedOffset && viewPoint < point {
+                                                }else if paddingValue.invertedOffset && viewPoint > point {
                                                     padding.1 -= point - viewPoint
                                                     return
                                                 }else if viewPoint == point {
